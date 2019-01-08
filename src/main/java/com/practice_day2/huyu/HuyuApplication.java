445869898cebbd8be3d63c9c7583eef501cb71d3
rickykennedy25 <13516105@std.stei.itb.ac.id>
2019-01-08 17:32:11 +0700

    init

---
+++ src/main/java/com/practice_day2/huyu/HuyuApplication.java
@@ -0,0 +1,14 @@
+package com.practice_day2.huyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuyuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HuyuApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/practice_day2/huyu/HuyuApplicationTests.java
@@ -0,0 +1,17 @@
+package com.practice_day2.huyu;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HuyuApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
